724c40a27c5422c1ed55c560c1af72ad999010c3
0baacon0 <160991594+0baacon0@users.noreply.github.com>
2024-03-15 11:53:05 +0800

    Update script.js

--- hw3/script.js
+++ hw3/script.js
@@ -1,7 +1,10 @@
 // 定義變數
 let canvas = document.getElementById("myCanvas");
 let ctx = canvas.getContext("2d");
-let x = 0,x2=300, y =0,y2=200, dx = 8,dx2=3, dy = 10,dy2=-4; r = 30,r2=70, color = "#0095DD",color2 = "#f9b80a",c1=0,c2=0,a=0;
+let x=100,y=220,x2=600,y2=200,
+ dx=7,dy=2,dx2=2,dy2=4,m=1,m2=4,
+r=70,r2=100,color="#0095DD",color2="#f9b80a",
+c1=0,c2=0,a=0,a2=0;
 
 // 畫圓形
 function drawBall(x, y, r, color)
@@ -28,24 +31,33 @@ function draw()
     x2 = x2 + dx2;
     y = y + dy;
     y2 = y2 + dy2;
-if(y>=570){dy=-5;c1=1;}
-else if(y<=30){dy=5;c1=1;}
-if(x>=770){dx=-5;c1=1;}
-else if(x<=30){dx=5;c1=1;}
+if(y>=600-r){dy=-dy;c1=1;}
+else if(y<=r){dy=-dy;c1=1;}
+
+if(x>=800-r){dx=-dx;c1=1;}
+else if(x<=r){dx=-dx;c1=1;}
+
 if(c1==1){color="#"+Math.floor(Math.random()*16777215).toString(16);
 c1=0;}
 
-if(y2>=530){dy2=-dy2;c2=1;}
-else if(y2<=70){dy2=-dy2;c2=1;}
-if(x2>=730){dx2=-dx2;c2=1;}
-else if(x2<=70){dx2=-dx2;c2=1;}
+if(y2>=600-r2){dy2=-dy2;c2=1;}
+else if(y2<=r2){dy2=-dy2;c2=1;}
+
+if(x2>=800-r2){dx2=-dx2;c2=1;}
+else if(x2<=r2){dx2=-dx2;c2=1;}
+
 if(c2==1){color2="#"+Math.floor(Math.random()*16777215).toString(16);
 c2=0;}
    
 
-if((x-x2)*(x-x2)+(y-y2)*(y-y2)<4*r*r2)
-{a=dx/2;dx=dx2*2;dx2=a;
-a=dy/2;dy=dy2*2;dy2=a;}
+
+if((x-x2)*(x-x2)+(y-y2)*(y-y2)<(r+r2)*(r+r2))
+{a=dx; a2=dy;
+
+c1=1;c2=1;
+dx=dx2;  dx2=a;
+dy=dy2;  dy2=a2;
+}
 
     drawBall(x, y, r, color);
     requestAnimationFrame(draw);
